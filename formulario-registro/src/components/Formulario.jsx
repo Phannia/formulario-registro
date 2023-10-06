bc3bb6729440ed72a30e37e817f7f963eec1a5fb
Phannia <lorenae.silvas@gmail.com>
2023-10-06 13:27:46 -0400

    structure modified

--- formulario-registro/src/components/Formulario.jsx
+++ formulario-registro/src/components/Formulario.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Formulario = () => {
+    const [nombre, setNombre] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [checkPassword, setCheckPassword] = useState('')
+    const [error, setError] = useState (false)
+    const validateData = (e) => {
+        e.preventDefault()
+
+        if (nombre === '' || email === '' || password === '' || checkPassword === '' ) 
+        {
+            setError(true);
+            return;
+        }
+        setError(false);
+        setNombre('');
+        setEmail('');
+        setPassword('');
+        setCheckPassword('');
+
+    }
   return (
-    <div>Formulario</div>
+    <div>
+        <form className='formulario' onSubmit={validateData}>
+            {error ? <p>Todos los campos son requeridos</p> : null}
+            <div className='form-group'>
+                <label>Nombre</label>
+                <input 
+                type='text' 
+                name='nombre'
+                className='form-control'
+                onChange={ (e) => setNombre(e.target.value) }
+                />
+            </div>
+            <div className='form-group'>
+                <label>Email</label>
+                <input 
+                type='text' 
+                name='email'
+                className='form-control'
+                onChange={ (e) => setEmail(e.target.value) }
+                />
+            </div>
+            <div className='form-group'>
+                <label>contraseña</label>
+                <input 
+                type='text' 
+                name='Contraseña'
+                className='form-control'
+                onChange={ (e) => setPassword(e.target.value) }
+                />
+            </div>
+            <div className='form-group'>
+                <label>Nombre</label>
+                <input 
+                type='text' 
+                name='check-contraseña'
+                className='form-control'
+                onChange={ (e) => setCheckPassword(e.target.value) }
+                />
+            </div>
+        </form>
+    </div>
   )
 }
 
