eac16d6997adec4df5c15b78903b3e2c2e71c43c
brvasquezf <brvasquezf@gmail.com>
2023-10-07 22:29:35 -0300

    modificacion de alert y css background

--- formulario-registro/src/components/SocialButton.jsx
+++ formulario-registro/src/components/SocialButton.jsx
@@ -1,13 +1,14 @@
 
 
-const SocialButton = ({icon}) => {
-  return (
-    <>
-      <div className='icon-box'>
-        <a className='icon-link'><i className={icon}></i></a>
-      </div>
-    </>
-  )
+const SocialButton = ({ icon }) => {
+    return (
+        <>
+
+            <button className="btn btn-outline-dark rounded-circle">
+                <a className='icon-link'><i className={icon}></i></a>
+            </button>
+        </>
+    )
 }
 
 export default SocialButton
